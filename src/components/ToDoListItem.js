ded7e370df0fb4145aaa7708a181793a675e6a92
fredriklunde <fredrik.lunde3@gmail.com>
2018-09-09 18:42:16 +0200

    lint fixes

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,35 +1,34 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { completeToDo } from "../actions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { completeToDo } from '../actions';
 
 class ToDoListItem extends Component {
-  handleCompleteClick = completeToDoId => {
-    const { completeToDo, auth } = this.props;
-    completeToDo(completeToDoId, auth.uid);
+  handleCompleteClick = (completeToDoId) => {
+      const { completeToDo, auth } = this.props;
+      completeToDo(completeToDoId, auth.uid);
   };
 
   render() {
-    const { todoId, todo } = this.props;
-    return (
-      <div key="toDoName" className="col s10 offset-s1 to-do-list-item teal">
-        <h4>
-          {todo.title}{" "}
-          <span
-            onClick={() => this.handleCompleteClick(todoId)}
-            className="complete-todo-item waves-effect waves-light teal lighten-5 teal-text text-darken-4 btn"
-          >
-            <i className="large material-icons">done</i>
-          </span>
-        </h4>
-      </div>
-    );
+      const { todoId, todo } = this.props;
+      return (
+          <div key="toDoName" className="col s10 offset-s1 to-do-list-item teal">
+              <h4>
+                  {todo.title}
+                  {' '}
+                  <span
+                      onClick={() => this.handleCompleteClick(todoId)}
+                      className="complete-todo-item waves-effect waves-light teal lighten-5 teal-text text-darken-4 btn"
+                  >
+                      <i className="large material-icons">done</i>
+                  </span>
+              </h4>
+          </div>
+      );
   }
 }
 
-const mapStateToProps = ({ auth }) => {
-  return {
-    auth
-  };
-};
+const mapStateToProps = ({ auth }) => ({
+    auth,
+});
 
 export default connect(mapStateToProps, { completeToDo })(ToDoListItem);
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,16 @@
-import * as firebase from "firebase";
+import * as firebase from 'firebase';
 
 const FirebaseConfig = {
-    apiKey:  process.env.REACT_APP_GOOGLE_FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_GOOGLE_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_GOOGLE_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_GOOGLE_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_GOOGLE_FIREBASE_PROJCECT_ID,
     storageBucket: process.env.REACT_APP_GOOGLE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_GOOGLE_FIREBASE_MESSAGING_SENDER_ID
-  };
+    messagingSenderId: process.env.REACT_APP_GOOGLE_FIREBASE_MESSAGING_SENDER_ID,
+};
 firebase.initializeApp(FirebaseConfig);
 
 const databaseRef = firebase.database().ref();
-export const todosRef = databaseRef.child("todos");
+export const todosRef = databaseRef.child('todos');
 export const authRef = firebase.auth();
 export const provider = new firebase.auth.GoogleAuthProvider();
