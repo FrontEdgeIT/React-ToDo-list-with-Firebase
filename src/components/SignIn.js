d72a54981d933d18a20132f4c5e0073f81d59070
fredriklunde <fredrik.lunde3@gmail.com>
2018-09-10 20:55:37 +0200

    added login form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -17,14 +17,27 @@ class Signin extends Component {
 
   render() {
       return (
-          <div className="row social-signin-container">
-              <div className="col s10 offset-s1 center-align">
-                  <img alt="Sign in" id="sign-in" src="/img/user.png" />
-                  <h4 id="sign-in-header">Sign In to start</h4>
-                  <a href="#" className="social-signin" onClick={this.props.signIn}>
-                      <i className="fa fa-google social-signin-icon" />
+          <div>
+              <div className="row social-signin-container">
+                  <div className="col s10 offset-s1 center-align">
+                      <img alt="Sign in" id="sign-in" src="/img/user.png" />
+                      <h4 id="sign-in-header">Sign In to start</h4>
+                      <a href="#" className="social-signin" onClick={this.props.signIn}>
+                          <i className="fa fa-google social-signin-icon" />
             Sign In With Google
-                  </a>
+                      </a>
+                  </div>
+              </div>
+              <div className="Login">
+                  <form onSubmit={this.handleSubmit}>
+                      <label>User Name</label>
+                      <input type="text" data-test="username" value="test" onChange={this.handleUserChange} />
+
+                      <label>Password</label>
+                      <input type="password" data-test="password" value="tes1t" onChange={this.handlePassChange} />
+
+                      <input type="submit" value="Log In" data-test="submit" />
+                  </form>
               </div>
           </div>
       );
