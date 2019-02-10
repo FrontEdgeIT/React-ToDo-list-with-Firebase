44eea533b77d396dd872e86c73afee958ed58d24
fredriklunde <fredrik.lunde3@gmail.com>
2019-02-10 09:23:30 +0100

    some fixes

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -10,8 +10,9 @@ class Signin extends Component {
   };
 
   componentWillUpdate(nextProps) {
+      const { router } = this.context;
       if (nextProps.auth) {
-          this.context.router.history.push('/app');
+          router.history.push('/app');
       }
   }
 
@@ -22,22 +23,11 @@ class Signin extends Component {
                   <div className="col s10 offset-s1 center-align">
                       <img alt="Sign in" id="sign-in" src="/img/user.png" />
                       <h4 id="sign-in-header">Sign In to start</h4>
-                      <a href="#" className="social-signin" onClick={this.props.signIn}>
+                      <button type="button" className="social-signin" onClick={this.props.signIn}>
                           <i className="fa fa-google social-signin-icon" />
             Sign In With Google
-                      </a>
+                      </button>
                   </div>
-              </div>
-              <div className="Login">
-                  <form onSubmit={this.handleSubmit}>
-                      <label>User Name</label>
-                      <input type="text" data-test="username" value="test" onChange={this.handleUserChange} />
-
-                      <label>Password</label>
-                      <input type="password" data-test="password" value="tes1t" onChange={this.handlePassChange} />
-
-                      <input type="submit" value="Log In" data-test="submit" />
-                  </form>
               </div>
           </div>
       );
